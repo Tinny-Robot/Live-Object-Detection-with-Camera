a2a728c472139da538d2576bd7c55d66fc0cc9b9
Nathaniel Handan <87228776+Tinny-Robot@users.noreply.github.com>
2023-09-18 05:54:33 +0100

    Update live.py

--- live.py
+++ live.py
@@ -1,8 +1,10 @@
+#Importing Libraries
 import cv2
 import supervision as sv
 from ultralytics import YOLO
 
 def live_object_detection():
+    '''main Function'''
     # Initialize the camera
     cap = cv2.VideoCapture(0)
     model = YOLO("yolov8n.pt")
